d33318b302b81bad1835e0ecf148897d0783227e
DZAKYALR <0dzakyalr@gmail.com>
2024-11-23 11:35:57 +0700

    update-generateImageUrl validation input
    Co-authored-by: Akbaroke <akbaroke@github.com>

--- backend/src/utils/generateImageUrl.js
+++ backend/src/utils/generateImageUrl.js
@@ -1,4 +1,8 @@
 export default function generateImageUrl(name) {
-  const slugname = name.toLowerCase().replace(/\s+/g, '+');
-  return `https://avatar.iran.liara.run/username?username=${slugname}`;
+  if (!name || typeof name !== 'string') {
+    throw new Error('Invalid name: Name must be a non-empty string.');
+  }
+
+  const slugName = encodeURIComponent(name.trim().toLowerCase().replace(/\s+/g, '+'));
+  return `https://avatar.iran.liara.run/username?username=${slugName}`;
 }
