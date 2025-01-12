b20388dc896190cc2c28a3ad6d643da5c2defc7a
akbaroke <akbarmuhammad833@gmail.com>
2025-01-12 16:56:56 +0700

    [update] menambahkan error alert pada page utama

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,45 +1,49 @@
-'use client';
-import { useEffect, useState } from 'react';
-import CardProduct from '@/components/molecules/CardProduct';
-import { BrandCountModel, ProductModel } from '@/interfaces/product';
-import { getBrands, getProducts } from '@/service/product';
-import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
-import { useSelector } from 'react-redux';
-import { StoreModel } from '@/interfaces/redux-model';
-import Spinner from '@/components/ui/spinner';
+"use client";
+import { useEffect, useState } from "react";
+import CardProduct from "@/components/molecules/CardProduct";
+import { BrandCountModel, ProductModel } from "@/interfaces/product";
+import { getBrands, getProducts } from "@/service/product";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { useSelector } from "react-redux";
+import { StoreModel } from "@/interfaces/redux-model";
+import Spinner from "@/components/ui/spinner";
+import { BiSolidError } from "react-icons/bi";
 
 export default function Home() {
   const [products, setProducts] = useState<ProductModel[]>([]);
   const [brands, setBrands] = useState<BrandCountModel[]>([]);
-  const [brandSelected, setBrandSelected] = useState<string>('');
+  const [brandSelected, setBrandSelected] = useState<string>("");
   const search = useSelector((state: StoreModel) => state.search);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const [productData, brandData] = await Promise.all([
-          getProducts(),
-          getBrands(),
-        ]);
-
-        setProducts(productData ?? []);
-        setBrands(brandData ?? []);
-      } catch (error) {
-        console.error('Failed to fetch data', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const [productData, brandData] = await Promise.all([
+        getProducts(),
+        getBrands(),
+      ]);
+
+      setProducts(productData ?? []);
+      setBrands(brandData ?? []);
+      setIsError(false);
+    } catch (error) {
+      console.error("Failed to fetch data", error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (search.value) {
-      setBrandSelected('');
+      setBrandSelected("");
     }
   }, [search.value]);
 
@@ -51,23 +55,49 @@ export default function Home() {
     ? products.filter((product) => product.brand === brandSelected)
     : products;
 
+  if (isError) {
+    return (
+      <div className="w-full max-w-[400px] rounded-2xl shadow-lg text-center bg-white mx-auto flex items-center justify-center flex-col p-10 gap-5 mt-5">
+        <div
+          className="p-3 rounded-full bg-red-500 text-white w-max shadow-md
+          animate-pulse transition-all duration-600
+          hover:scale-110 hover:shadow-lg"
+        >
+          <BiSolidError size={50} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-xl font-bold">Server Error</h1>
+          <p className="text-muted-foreground italic text-sm leading-6">
+            Our server is currently down, please try again later. We apologize
+            for the inconvenience.
+          </p>
+        </div>
+        <Button onClick={() => window.location.reload()} variant="outline">
+          Refresh
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-[1500px] mx-auto px-5 relative w-full">
       <div className="mb-10 flex flex-wrap items-center gap-3">
         {brands.map((brand) => (
           <Button
-            variant={brandSelected === brand.name ? 'default' : 'outline'}
+            variant={brandSelected === brand.name ? "default" : "outline"}
             key={brand.name}
             className="flex-shrink-0"
             onClick={() =>
               setBrandSelected((prev) =>
-                prev === brand.name ? '' : brand.name
+                prev === brand.name ? "" : brand.name
               )
-            }>
-            {brand.name}{' '}
+            }
+          >
+            {brand.name}{" "}
             <Badge
               className="px-1.5 py-[0.5px] ml-2 text-sm"
-              variant="secondary">
+              variant="secondary"
+            >
               {brand.count}
             </Badge>
           </Button>
